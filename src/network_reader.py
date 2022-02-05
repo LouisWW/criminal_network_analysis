5bfde515ff60e6d460cc6c1f744ab4787c40dd8d
LouisWW <louis.weyland@hotmail.com>
2022-02-05 19:28:34 +0100

    Pylinted

--- src/network_reader.py
+++ src/network_reader.py
@@ -5,77 +5,69 @@ __author__ = Louis Weyland
 __date__   = 5/02/2022
 """
 import os
-import networkx as nx
-import networkit as nxk
-import pandas as pd
-import matplotlib.pyplot as plt
 
+import matplotlib.pyplot as plt
+import networkx as nx
+import pandas as pd
 
 
 class NetworkReader():
     """
-    The NetworkReader reads the data from various files which have 
-    various structures and returns an a readable structure for the 
+    The NetworkReader reads the data from various files which have
+    various structures and returns an a readable structure for the
     networkit
     """
-    
+
     def __init__(self):
         """set the directory right"""
-        self.directory = os.path.dirname(os.path.realpath(__file__))+'/data/'
-        
-        
+        self.directory = os.path.dirname(os.path.realpath(__file__)) + '/data/'
+
     def read_cunha(self):
         """
         Reads the data from the following paper
         https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6214327/
         """
-        data = pd.read_csv(self.directory+'Cunha2018.csv',delimiter=';',
-                                names=['vertex1','vertex2'])
-        graph_obj = nx.Graph(list(zip(data['vertex1'],data['vertex2'])))
+        data = pd.read_csv(self.directory + 'Cunha2018.csv', delimiter=';',
+                            names=['vertex1', 'vertex2'])
+        graph_obj = nx.Graph(list(zip(data['vertex1'], data['vertex2'])))
         return graph_obj
-    
+
     def read_montagna_meetings(self):
         """
-        Reads the data from the following paper 
+        Reads the data from the following paper
         https://zenodo.org/record/3938818#.Yf64mPso9FE
         Pieces of code has been taken from the according github repository
         https://github.com/lcucav/networkdisruption
         """
-        data = pd.read_csv(self.directory+'Montagna_Meetings_Edgelist.csv', sep = '\s+')
+        data = pd.read_csv(
+            self.directory +
+            'Montagna_Meetings_Edgelist.csv',
+            sep='\\s+')
         
-        graph_obj = nx.from_pandas_edgelist(data, source='Source', target='Target', 
-                                                    edge_attr=['Weight'])
+        graph_obj = nx.from_pandas_edgelist(
+            data, source='Source', target='Target', edge_attr=['Weight'])
         return graph_obj
 
-        
     def read_montagna_phone_calls(self):
         """
-        Reads the data from the following paper 
+        Reads the data from the following paper
         https://zenodo.org/record/3938818#.Yf64mPso9FE
         Pieces of code has been taken from the according github repository
         https://github.com/lcucav/networkdisruption
         """
-        data = pd.read_csv(self.directory+'Montagna_Phone_Calls_Edgelist.csv', sep = '\s+')
-    
-        graph_obj = nx.from_pandas_edgelist(data, source='Source', target='Target', 
-                                                    edge_attr=['Weight'])
+        data = pd.read_csv(
+            self.directory +
+            'Montagna_Phone_Calls_Edgelist.csv',
+            sep='\\s+')
+
+        graph_obj = nx.from_pandas_edgelist(
+            data, source='Source', target='Target', edge_attr=['Weight'])
         return graph_obj
 
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-if __name__=='__main__':
-    
 
-    network_reader= NetworkReader()
+if __name__ == '__main__':
+
+    network_reader = NetworkReader()
     print(network_reader.directory)
     network_obj = network_reader.read_cunha()
     nx.draw(network_obj)
