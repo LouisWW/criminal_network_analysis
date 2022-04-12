5a6f466d59f6a05261eb05a74eac3553fb330936
LouisWW <louis.weyland@hotmail.com>
2022-04-12 16:30:49 +0200

    Added group division and init func

--- src/simulators/sim_mart_vaq.py
+++ src/simulators/sim_mart_vaq.py
@@ -7,6 +7,9 @@ __author__ = Louis Weyland
 __date__   = 11/04/2022
 """
 import logging
+from typing import FrozenSet
+from typing import List
+from typing import Tuple
 
 import graph_tool.all as gt
 import numpy as np
@@ -20,9 +23,17 @@ class SimMartVaq:
     """Contain the framework to simulate the process."""
 
     def __init__(
-        self, network: gt.Graph, ratio_honest: float = 0.7, ratio_wolf: float = 0.1
+        self,
+        network: gt.Graph,
+        ratio_honest: float = 0.7,
+        ratio_wolf: float = 0.1,
+        n_new_edges: int = 2,
     ) -> None:
         """Init the network charaterisics."""
+        # Define name of simulator
+        self._name = "sim_mart_vaq"
+        self.network = network
+
         # Check if data is coherent
         assert isinstance(network, gt.Graph), "Network should be of type gt."
         assert 0 < ratio_honest < 1, "Ratio needs to be (0,1)"
@@ -36,19 +47,11 @@ class SimMartVaq:
 
         # Network needs to have a base criminal network
         self.n_criminal = len(gt.find_vertex(network, network.vp.state, "c"))
+        assert self.n_criminal > 1, "The given network contains no criminals..."
 
         self.total_number_nodes = int(self.n_criminal / self.ratio_criminal)
         self.new_nodes = self.total_number_nodes - self.n_criminal
-        logger.info(
-            f"Given the ratio param, {self.new_nodes} \
-                    nodes are added, total = {self.total_number_nodes} nodes!"
-        )
 
-        self.new_network = NetworkCombiner.combine_by_preferential_attachment_faster(
-            network, new_nodes=self.new_nodes, n_new_edges=2
-        )
-
-        # Get all the agents with no states
         # Init either honest or lone wolf
         self.relative_ratio_honest = round(
             self.ratio_honest / (self.ratio_honest + self.ratio_wolf), 2
@@ -56,10 +59,56 @@ class SimMartVaq:
         self.relative_ratio_wolf = round(
             self.ratio_wolf / (self.ratio_honest + self.ratio_wolf), 2
         )
-        nodes_no_states = gt.find_vertex(network, network.vp.state, "")
+
+    @property
+    def name(self) -> str:
+        """Return the name of the simulator."""
+        return self._name
+
+    def initialise_network(self, network: gt.Graph, n_new_edges: int = 2) -> gt.Graph:
+        """Add to the existing criminal network honests and lone wolfs.
+
+        Thereby, the nodes are added based on the preferential attachment principle.
+        Returns a network with new added nodes respecting the ratio of criminals/honest/wolfs.
+        """
+        logger.info(
+            f"Given the ratio param, {self.new_nodes} \
+                    nodes are added, total = {self.total_number_nodes} nodes!"
+        )
+        new_network = NetworkCombiner.combine_by_preferential_attachment_faster(
+            network, new_nodes=self.new_nodes, n_new_edges=n_new_edges
+        )
+
+        # Get all the agents with no states
+        nodes_no_states = gt.find_vertex(new_network, new_network.vp.state, "")
         for i in tqdm(
-            nodes_no_states, desc="Adding attriutes to nodes", total=self.new_nodes
+            nodes_no_states, desc="Adding attributes to nodes", total=self.new_nodes
         ):
-            self.new_network.vp.state[self.new_network.vertex(i)] = np.random.choice(
+            new_network.vp.state[new_network.vertex(i)] = np.random.choice(
                 ["h", "w"], 1, p=[self.relative_ratio_honest, self.relative_ratio_wolf]
-            )
+            )[0]
+
+        return new_network
+
+    def acting_stage(self, network: gt.Graph) -> None:
+        """Correspond to the acting stage in the paper.
+
+        Network is subdivided in to n groups.
+        In each group, a person is selected.
+        If selected person is a wolf or criminal,
+        damage is inflicted on others.
+        """
+        pass
+
+    def divide_in_groups(self, network: gt.Graph) -> Tuple[List[int], FrozenSet[int]]:
+        """Divide the network in groups.
+
+        Making use of the  minimize_blockmodel_dl func.
+        For now, the number of groups can't be imposed.
+        Returns a list with the group number/label.
+        """
+        partitions = gt.minimize_blockmodel_dl(network)
+        groups = partitions.get_blocks()
+        groups_label = frozenset(groups)
+
+        return groups, groups_label
--- src/test/tests_simulators/test_sim_mart_vaq.py
+++ src/test/tests_simulators/test_sim_mart_vaq.py
@@ -1,6 +1,7 @@
 """Test if the simulation from Martinez-Vaquero is running correctly."""
 import graph_tool.all as gt
 import pytest
+from numpy import empty
 from simulators.sim_mart_vaq import SimMartVaq
 
 
@@ -32,3 +33,62 @@ class TestSimMartVaq:
         assert simulators.new_nodes == 63, "Number of nodes to add is wrong"
         assert simulators.relative_ratio_honest == 0.75, "Relative ratio is wrong"
         assert simulators.relative_ratio_wolf == 0.25, "Relative ratio is worng"
+
+        # Try to change its name
+        with pytest.raises(Exception):
+            simulators.name = "New name"
+
+    @pytest.mark.essential
+    def test_sim_mart_vaq_wrong_init(self, gt_network: gt.Graph) -> None:
+        """Test if a wrong init triggers the assert statments."""
+        # With ratio_honest == 0
+        with pytest.raises(Exception):
+            assert SimMartVaq(gt_network, ratio_honest=0)
+
+        # With ratio_wolf == 1.1
+        with pytest.raises(Exception):
+            assert SimMartVaq(gt_network, ratio_wolf=1.1)
+
+        # With ratios not adding up more than 1
+        with pytest.raises(Exception):
+            assert SimMartVaq(gt_network, ratio_honest=0.8, ratio_wolf=0.3)
+            assert SimMartVaq(gt_network, ratio_honest=0.8, ratio_wolf=0.2)
+            assert SimMartVaq(gt_network, ratio_honest=-0.8, ratio_wolf=0.1)
+            assert SimMartVaq(gt_network, ratio_honest=-0.8, ratio_wolf=0.2)
+
+    @pytest.mark.essential
+    def test_initialise_network(self, gt_network: gt.Graph) -> None:
+        """Test if the init process is done corretly.
+
+        More precisely tests if the ratio of c/h/w is correct
+        """
+        simulators = SimMartVaq(gt_network, ratio_honest=0.2, ratio_wolf=0.3)
+        network = simulators.initialise_network(simulators.network)
+
+        # Criminal network size should be 95
+        # Honest and Wolf ration should be within a range given the init is stochastic
+        assert (
+            len(gt.find_vertex(network, network.vp.state, "c")) == 95
+        ), "Criminal ratio not correct"
+        assert (
+            38 - 5 <= len(gt.find_vertex(network, network.vp.state, "h")) <= 38 + 5
+        ), "Honest ratio not correct"
+        assert (
+            57 - 5 <= len(gt.find_vertex(network, network.vp.state, "w")) <= 57 + 5
+        ), "Wolf ratio not correct"
+
+    @pytest.mark.essential
+    def test_divide_in_groups(self, gt_network: gt.Graph) -> None:
+        """Test if the division into groups works correctly."""
+        simulators = SimMartVaq(gt_network)
+        groups, groups_label = simulators.divide_in_groups(simulators.network)
+
+        assert groups is not empty, "Group list is empty"
+        assert all(
+            [isinstance(item, int) for item in groups]
+        ), "Items in Group should be int"
+        assert isinstance(groups_label, frozenset), "Group label should be frozenset"
+        assert len(groups_label) > 0, "Group label is empty"
+        assert all(
+            [isinstance(item, int) for item in groups_label]
+        ), "Items in Group label should be int"
