77853db8f0b0178eb8727ad471b3be4dff4cd1f3
LouisWW <louis.weyland@hotmail.com>
2022-05-08 15:20:50 +0200

    Added pipeline

--- src/bash_script.sh
+++ src/bash_script.sh
@@ -10,4 +10,4 @@
 
 
 # Run the sensitivity analysis
-nohub python3 main.py -read-data montagna_meetings -sa sim-mart-vaq -n-samples 32 -r 2500 -output-value ratio_criminal -save > bash_script_log 2>&1
+python3 main.py -read-data montagna_meetings -sa sim-mart-vaq -n-samples 32 -r 2500 -output-value ratio_criminal -save
